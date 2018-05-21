b92bbb8a83b73086bfcb1a2b098312648358eb4c
Youbaraj <y.poudel@inspheris.com>
2018-05-21 19:13:16 +0200

    Project Initialized

---
+++ app/src/main/java/schooldemo/prototype/insperis/fr/schoolapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package schooldemo.prototype.insperis.fr.schoolapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
